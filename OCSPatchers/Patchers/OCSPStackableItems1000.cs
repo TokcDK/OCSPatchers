72f78ac0af48db2ff808f04505f95f86a624f517
TokcDK <TokcDK@gmail.com>
2024-03-26 13:23:13 +0500

    patch storages also

--- OCSPatchers/Patchers/OCSPStackableItems1000.cs
+++ OCSPatchers/Patchers/OCSPStackableItems1000.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using OpenConstructionSet;
 using OpenConstructionSet.Data;
 using OpenConstructionSet.Installations;
+using OpenConstructionSet.Mods;
 using OpenConstructionSet.Mods.Context;
 
 namespace OCSPatchers.Patchers
@@ -19,15 +20,30 @@ namespace OCSPatchers.Patchers
             var items = context.Items.OfType(ItemType.Item);
             foreach (var item in items)
             {
-                if (!item.Values.TryGetValue("stackable", out var value)) continue;
-                if (!item.Values.TryGetValue("slot", out var slotValue)) continue;
-                if (slotValue is not int i || i != 7) continue; // attach slot none
-                if (value is not int v || v >= 1000) continue; // already max
-
-                //Console.WriteLine("Updating " + item.Name);
-                item.Values["stackable"] = 1000;
+                if (!CheckAsItem(item)) 
+                    CheckAsStorage(item);
             }
             return Task.CompletedTask;
+        }
+
+        private void CheckAsStorage(ModItem item)
+        {
+            if (!item.Values.ContainsKey("stackable bonus mult")) return;
+
+            item.Values["stackable bonus mult"] = 1000;
+        }
+
+        private bool CheckAsItem(ModItem item)
+        {
+            if (!item.Values.TryGetValue("stackable", out var value)) return false;
+            if (!item.Values.TryGetValue("slot", out var slotValue)) return false;
+            if (slotValue is not int i || i != 7) return false; // attach slot none
+            if (value is not int v || v >= 1000) return false; // already max
+
+            //Console.WriteLine("Updating " + item.Name);
+            item.Values["stackable"] = 1000;
+
+            return true;
         }
     }
 }
