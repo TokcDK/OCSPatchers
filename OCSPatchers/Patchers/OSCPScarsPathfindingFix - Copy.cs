e379a971b9f69b84f0aea7bda31e5f1051733baf
TokcDK <TokcDK@gmail.com>
2024-03-29 18:23:49 +0500

    add OSCPNPCEnjoysMoreShopping

---
+++ OCSPatchers/Patchers/OSCPScarsPathfindingFix - Copy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenConstructionSet;
+using OpenConstructionSet.Data;
+using OpenConstructionSet.Installations;
+using OpenConstructionSet.Mods;
+using OpenConstructionSet.Mods.Context;
+
+namespace OCSPatchers.Patchers
+{
+    internal class OSCPNPCEnjoysMoreShopping : OCSPatcherBase
+    {
+        public override string PatcherName => "NPCEnjoysMoreShopping mod tweaks for mods";
+
+        public override string[] ReferenceModNames => new[] { "NPC enjoys more shopping.mod" };
+
+        public override async Task ApplyPatch(IModContext context, IInstallation installation)
+        {
+            var characters = context.Items.OfType(ItemType.Character).Where(i => !i.IsDeleted());
+            Parallel.ForEach(characters, character =>
+            {
+                if (!character.Values.ContainsKey("wages")) return;
+
+                character.Values["wages"] = 10000;
+            });
+        }
+    }
+}
