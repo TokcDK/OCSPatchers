6ebdfcccdf101b66550812e88fbcc387eeed89f3
TokcDK <TokcDK@gmail.com>
2024-10-20 13:57:41 +0500

    add more items to storage fish

---
+++ OCSPatchers/Patchers/MoreItemsToSpecificStorage/OCSMoreItemsToSpecificStorageFish.cs
@@ -0,0 +1,41 @@
+﻿using OpenConstructionSet;
+using OpenConstructionSet.Data;
+using OpenConstructionSet.Installations;
+using OpenConstructionSet.Mods;
+using OpenConstructionSet.Mods.Context;
+
+namespace OCSPatchers.Patchers.MoreItemsToSpecificStorage
+{
+    internal class OCSMoreItemsToSpecificStorageFish : OCSMoreItemsToSpecificStorageBase
+    {
+        public override string PatcherName => "Add more fish products for fish products storages";
+
+        protected override string[] LimitInventoryItemIdsToCheck => new string[2] 
+        {
+            "98578-ModPack.mod", // сырая рыба
+            "50518-Newwworld.mod" // вяленая рыба
+        };
+        protected override bool IsValidItemSpecific(ModItem item)
+        {
+            return IconHaveFishName(item) || _validNameKeywords.Any(i => item.Name.Contains(i, StringComparison.InvariantCultureIgnoreCase));
+        }
+
+        private bool IconHaveFishName(ModItem item)
+        {
+            if (!item.Values.TryGetValue("icon", out var value)) return false;
+            if (value is not string v || !(_validNameKeywords.Any(i => v.Contains(i, StringComparison.InvariantCultureIgnoreCase)))) return false;
+
+            return true;
+        }
+
+        readonly List<string> _validNameKeywords = new List<string>()
+        {
+            "fish",
+            "рыба",
+            "рыбища",
+            "рыбёшка",
+        };
+
+        protected override int ItemFunctionIdToAdd => (int)Data.Enums.ItemFunction.ITEM_FOOD;
+    }
+}
---
+++ OCSPatchers/Patchers/MoreItemsToSpecificStorage/OCSMoreItemsToSpecificStorageFish1.cs
@@ -0,0 +1,35 @@
+﻿using OpenConstructionSet;
+using OpenConstructionSet.Data;
+using OpenConstructionSet.Installations;
+using OpenConstructionSet.Mods;
+using OpenConstructionSet.Mods.Context;
+
+namespace OCSPatchers.Patchers.MoreItemsToSpecificStorage
+{
+    internal class OCSMoreItemsToSpecificStorageFish1 : OCSMoreItemsToSpecificStorageFish
+    {
+        public override string PatcherName => "Add more raw fish for raw fish storages";
+
+        protected override string[] LimitInventoryItemIdsToCheck => new string[2] 
+        {
+            "50514-Newwworld.mod", // рыбища
+            "50517-Newwworld.mod" // рыбешка
+        };
+
+        protected override bool IsValidItemSpecific(ModItem item)
+        {
+            return HaveNoIngredients(item) && !_meatNameKeywords.Any(i => item.Name.Contains(i, StringComparison.InvariantCultureIgnoreCase)) && base.IsValidItemSpecific(item);
+        }
+        readonly List<string> _meatNameKeywords = new List<string>()
+        {
+            "meat",
+            "мясо",
+            "мясн",
+        };
+
+        private bool HaveNoIngredients(ModItem item)
+        {
+            return !item.ReferenceCategories.ContainsKey("ingredients");
+        }
+    }
+}
